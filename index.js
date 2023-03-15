726ffe2c3c4bd4ca887c2d7525e6bad4c39e5bd4
SergeyDedikov <serded59@yandex.ru>
2023-03-15 15:33:43 +0500

    feat: formate date and refactor validation

--- index.js
+++ index.js
@@ -2,34 +2,24 @@
 const form = document.getElementById('new-comment');
 const comments = document.getElementById('comments');
 
-// валидация формы
-form.name.onblur = showErrors;
-form.text.onblur = showErrors;
-form.name.onfocus = hideErrors;
-form.text.onfocus = hideErrors;
-
 function showErrors() {
+  // если поля пустые
+  if (this.value === '') {
+    switch (this.name) {
+      case 'name':
+        setMessage(this, 'Имя должно быть указано');
+        break;
 
-  switch (this.name) {
-    case 'name':
-      if (!this.value.includes('@')) { // нет знака email
-        this.classList.add('invalid');
-        this.nextElementSibling.textContent = 'Имя должно содержать знак @';
-      }
-      if (this.value === '') {
-        this.classList.add('invalid');
-        this.nextElementSibling.textContent = 'Имя должно быть указано';
-      }
-      break;
-
-    case 'text':
-      if (this.value === '') {
-        this.classList.add('invalid');
-        this.nextElementSibling.textContent = 'Сообщение должно быть заполнено';
-      }
-      break;
+      case 'text':
+        setMessage(this, 'Сообщение должно быть заполнено');
+        break;
+    }
   }
+}
 
+function setMessage(elem, message) {
+  elem.classList.add('invalid');
+  elem.nextElementSibling.textContent = message;
 }
 
 function hideErrors() {
@@ -37,20 +27,6 @@ function hideErrors() {
     this.classList.remove('invalid');
     this.nextElementSibling.textContent = '';
   }
-}
-
-form.onsubmit = function (e) {
-  console.log('Submit!');
-
-  let inputData = {
-    name: form.name.value,
-    text: form.text.value,
-    date: form.date.value,
-  };
-
-  comments.prepend(createComment(inputData));
-
-  return false;
 }
 
 function createComment(data) {
@@ -94,6 +70,75 @@ function toggleLike(e) {
   target.classList.toggle('liked');
 }
 
+function formateDate(date) {
+  let string = '';
+
+  let now = new Date();
+  let inputDate = new Date(Date.parse(date));
+
+  function isTodayOrYesterday() {
+
+    let timeDiff = now.setHours(0, 0, 0, 0) - inputDate.setHours(0, 0, 0, 0);
+
+    // если дата не введена или даты совпадают
+    if (date == '' || timeDiff == 0) return 'today';
+    // если между текущей и введённой датой 24 часа
+    if (timeDiff == 24 * 60 * 60 * 1000) return 'yesterday';
+
+  }
+
+  let day = isTodayOrYesterday();  // "вчера" или "сегодня", либо undefined
+
+  now = new Date();
+  // получаем строками часы и минуты, добавляем 0 впереди
+  let strHours = ('0' + now.getHours()).slice(-2);
+  let strMinutes = ('0' + now.getMinutes()).slice(-2);
+  let strTime = `${strHours}:${strMinutes}`;
+
+  // итоговая строка
+  switch (day) {
+    case 'today':
+      string = 'Сегодня, ' + strTime;
+      break;
+
+    case 'yesterday':
+      string = 'Вчера, ' + strTime;
+      break;
+
+    default:
+      string = date;
+      break;
+  }
+
+  return string;
+}
+
+// Events Listeners
+
+// валидация формы
+form.name.onblur = showErrors;
+form.text.onblur = showErrors;
+form.name.onfocus = hideErrors;
+form.text.onfocus = hideErrors;
+
+// клики по иконкам
 comments.addEventListener('click', deleteComment);
 comments.addEventListener('click', toggleLike);
 
+// отправка формы (вставка сообщения)
+form.onsubmit = function (e) {
+  console.log('Submit!');
+
+  let inputData = {
+    name: this.name.value,
+    text: this.text.value,
+    date: this.date.value,
+  };
+
+  inputData.date = formateDate(inputData.date);
+
+  comments.prepend(createComment(inputData));
+
+  this.reset();
+  return false;
+}
